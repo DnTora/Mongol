861bc2eae47dc40f9a935d2cf237021f2e739d6a
naor <naorpio15@gmail.com>
2017-06-29 19:18:28 +0300

    Add files via upload

--- NewSitePHP/connect.php
+++ NewSitePHP/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$conect = mysqli_connect('localhost', 'id2047781_mybookssite', '12345678', 'id2047781_users');
+	$connect = mysqli_connect('localhost', 'id2047781_mybookssite', '12345678', 'id2047781_users');
 	if(mysqli_connect_errno()) {
 		die("Conection Failed: " . mysqli_connect_error());
 	}
