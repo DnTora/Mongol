47cb955c0272cc9afd20674c31f35557c74de1eb
naor <naorpio15@gmail.com>
2017-07-02 20:57:28 +0300

    Add files via upload

---
+++ NewSitePHP - version 2/contact.php
@@ -0,0 +1,10 @@
+<?php
+	include_once "HTML - static content/htmlHead.html";
+	include_once "HTML - static content/htmlBody_header.html";
+	include_once "HTML - static content/htmlBody_main_rightToolbar_loginArea.html";
+	include_once "HTML - static content/htmlBody_main_rightToolbar_whatsNew.html";
+	////////////////////////////////////////////////////// Page content file //////////////////////////////////////////////////////
+	include_once "HTML - pages content/contact.html";
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	include_once "HTML - static content/htmlBody_footer.html";
+?>
