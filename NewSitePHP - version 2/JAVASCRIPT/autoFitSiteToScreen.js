21056664857b725fc7fa78364e6847c0d731171e
naor <naorpio15@gmail.com>
2017-07-03 23:32:38 +0300

    Delete autoFitSiteToScreen.js

--- NewSitePHP - version 2/JAVASCRIPT/autoFitSiteToScreen.js
+++
@@ -1,8 +0,0 @@
-var CONTAINER_PERCENTAGE = 0.8;
-var containerSize = window.outerWidth * CONTAINER_PERCENTAGE;
-var paddingDistance = (window.outerWidth - window.outerWidth * CONTAINER_PERCENTAGE) / 2;
-document.getElementById("div_container").style.padding = "0px " + paddingDistance + "px " + "0px " + paddingDistance + "px";
-document.getElementById("div_header").style.width = containerSize + "px";
-document.getElementById("div_navigationBar").style.width = containerSize + "px";
-document.getElementById("div_main").style.width = containerSize + "px";
-document.getElementById("div_footer").style.width = containerSize + "px";
