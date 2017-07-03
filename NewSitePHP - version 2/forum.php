ffa1009208ca2f4b66ddae0e34d03131ee760f6b
naor <naorpio15@gmail.com>
2017-07-03 23:14:38 +0300

    Add files via upload

---
+++ NewSitePHP - version 2/forum.php
@@ -0,0 +1,11 @@
+<?php
+	include_once "PHP - HTML static content/htmlHead.php";
+	include_once "PHP - HTML static content/htmlBody_header.php";
+	include_once "PHP - HTML static content/htmlBody_main_rightToolbar_loginArea.php";
+	include_once "PHP - HTML static content/htmlBody_main_rightToolbar_whatsNew.php";
+	////////////////////////////////////////////////////// Page content file //////////////////////////////////////////////////////
+	include_once "PHP - HTML pages content/forum.php";
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	include_once "PHP - HTML static content/htmlBody_footer.php";
+	include_once "PHP - HTML static content/htmlBody_bottom.php";
+?>
