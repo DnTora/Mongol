a56d5a5d9e27113777ac64e517aafbeb7e240231
naor <naorpio15@gmail.com>
2017-07-04 23:04:08 +0300

    Add files via upload

--- NewSitePHP - version 2/PHP/login.php
+++ NewSitePHP - version 2/PHP/login.php
@@ -1,25 +1,32 @@
 <?php
 	session_start();
-	if(isset($_POST['submit'])) {
+	if(!isset($_POST['submit'])) {
+		setcookie("login_errorData", "שימו לב: אירעה שגיאה!", time() + 1, "/");
+		header('Location: ../index.php?login=error');
+		exit();
+	} else {
 		include 'databaseConnect.php';
 		$userName = mysqli_real_escape_string($databaseConnect, $_POST['userName']);
 		$password = mysqli_real_escape_string($databaseConnect, $_POST['password']);
 		if(empty($userName) || empty($password)) {
+			setcookie("login_errorData", "שימו לב: אחד או יותר משדות החובה לא מולאו!", time() + 1, "/");
 			header('Location: ../index.php?login=empty');
 			exit();
 		} else {
 			$sql = "SELECT * FROM users WHERE user_name='$userName'";
 			$result = mysqli_query($databaseConnect, $sql);
 			$checkRow = mysqli_num_rows($result);
-			if($checkRow < 1) {
-				header('Location: ../index.php?login=ntexist');
+			if($checkRow == 0) {
+				setcookie("login_errorData", "שימו לב: שם המשתמש לא נמצא במערכת!", time() + 1, "/");
+				header('Location: ../index.php?login=userNameNotFound');
 				exit();
 			} elseif($row = mysqli_fetch_assoc($result)) {
 				$hashPass = password_verify($password, $row['password']);
-				if($hashPass == false) { 
-					header('Location: ../index.php?login=error');
+				if(!$hashPass) {
+					setcookie("login_errorData", "שימו לב: הסיסמה שגויה!", time() + 1, "/");
+					header('Location: ../index.php?login=wrongPassword');
 					exit();
-				} elseif($hashPass == true) {
+				} else {
 					$_SESSION['userName'] = $row['userName'];
 					$_SESSION['password'] = $row['password'];
 					$_SESSION['email'] =  $row['email'] ;
@@ -28,8 +35,5 @@
 				}
 			}
 		}
-	} else {
-		header('Location: ../index.php?login=ntclick');
-		exit();
 	}
 ?>
--- NewSitePHP - version 2/PHP/register.php
+++ NewSitePHP - version 2/PHP/register.php
@@ -1,6 +1,7 @@
 <?php
 	if(!isset($_POST['submit'])) {
-		header('Location: ../index.php?register=ntclick');
+		setcookie("registeration_errorData", "שימו לב: אירעה שגיאה!", time() + 1, "/");
+		header('Location: ../index.php?register=error');
 		exit();
 	} else {
 		include_once 'databaseConnect.php';
@@ -11,28 +12,29 @@
 		$familyName = mysqli_real_escape_string($databaseConnect , $_POST['familyName']);
 		$email = mysqli_real_escape_string($databaseConnect, $_POST['email']);
 		if(empty($userName) || empty($password) || empty($passwordRepeat)) {
+			setcookie("registeration_errorData", "שימו לב: אחד או יותר משדות החובה לא מולאו!", time() + 1, "/");
 			header('Location: ../index.php?register=empty');
 			exit();
 		} elseif($password!==$passwordRepeat) {
-			setcookie("errg","שגיאה:האימות סיסמה לא תואמת ", time() + 1, "/");
-			header('Location: ../index.php?register=passer');
+			setcookie("registeration_errorData", "שימו לב: הסיסמאות לא תואמות!", time() + 1, "/");
+			header('Location: ../index.php?register=passwordsMismatch');
 			exit();
-		} elseif(!preg_match("/([a-zA-Z0-9])/", $userName)) {
-			header('Location: ../index.php?register=name1eror');
-			exit();
-		} elseif(strlen($userName) <= 3) {
-			header('Location: ../index.php?register=name2eror');
+		} elseif(!preg_match("/([a-zA-Z0-9])/", $userName) || strlen($userName) <= 2) {
+			setcookie("registeration_errorData", "שימו לב: שם המשתמש חייב להכיל רק אותיות באנגלית ומספרים ואורכו צריך להיות 3 אותיות ומעלה!", time() + 1, "/");
+			header('Location: ../index.php?register=invalidName');
 			exit();
 		} elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			header('Location: ../index.php?register=emaileror');
-			exit();	
+			setcookie("registeration_errorData", "שימו לב: האימייל לא תקין!", time() + 1, "/");
+			header('Location: ../index.php?register=invalidEmail');
+			exit();
 		} else {
 			$sql = "SELECT * FROM users WHERE user_name='$userName'";
 			$result = mysqli_query($databaseConnect, $sql);
 			$rowNum = mysqli_num_rows($result);
 			if($rowNum > 0) {
-				header('Location: ../index.php?register=namecatch');
-				exit();	
+				setcookie("registeration_errorData", "שימו לב: שם המשתמש תפוס!", time() + 1, "/");
+				header('Location: ../index.php?register=userNameExist');
+				exit();
 			} else {
 				$hashPass = password_hash($password, PASSWORD_DEFAULT);
 				$sql = "INSERT INTO users (user_name, password, private_name, family_name, email) VALUES('$userName', '$hashPass', '$privateName', '$familyName', '$email')";
