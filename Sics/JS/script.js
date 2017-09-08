e1f32b3bd42cfa54fd4ae96bd71230719581bfd0
naor <naorpio15@gmail.com>
2017-09-08 19:52:57 +0300

    Add files via upload

---
+++ Sics/JS/script.js
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////// Methods /////////////////////////////////////////////////////////////
+function togglePopUpWindowAreaVisibility(windowAreaDivName) {
+	var popUpWindow = document.getElementById(windowAreaDivName);
+	if (popUpWindow.style.display == "")
+		popUpWindow.style.display = "block";
+	else
+		popUpWindow.style.display = "";
+}
+
+function getCookieValue(cookieName) {
+	var cookiesArray = (document.cookie).split("; ");
+	for (i = 0; i < cookiesArray.length; i++) {
+		var cookieDetailsArray = cookiesArray[i].split("=");
+		if (cookieDetailsArray[0] == cookieName)
+			return cookieDetailsArray[1];
+	}
+	return null;
+}
+
+function deleteCookie(cookieName) {
+	document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT';
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+///////////////////////////////////////////////////////////// Events //////////////////////////////////////////////////////////////
+window.onload = function(event) {
+	deleteCookie("login_status");
+	if (getCookieValue("registeration_status") != null) {
+		togglePopUpWindowAreaVisibility("div_registerationPopUp");
+		deleteCookie("registeration_status");
+	}
+	deleteCookie("order_status");
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
