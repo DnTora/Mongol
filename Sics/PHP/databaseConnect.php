c26b428e819a7a352a4126541e9bdaf58a5e46d4
naor <naorpio15@gmail.com>
2017-09-08 20:18:45 +0300

    Delete databaseConnect.php

--- Sics/PHP/databaseConnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$databaseConnect = mysqli_connect('localhost', 'id2031617_testdatabase', '475869123a', 'id2031617_sitedatabase');
-	if(mysqli_connect_errno()) {
-		die("Conection Failed: " . mysqli_connect_error());
-	}
-?>
