85ecb269e5345cc76764bf2dea35c413be40de0c
naor <naorpio15@gmail.com>
2017-09-08 19:50:02 +0300

    Add files via upload

---
+++ Sics/index.php
@@ -0,0 +1,10 @@
+<?php
+	include_once "PHP - HTML static content/htmlHead.php";
+	include_once "PHP - HTML static content/htmlBody_header.php";
+	include_once "PHP - HTML static content/htmlBody_main_rightToolbar_loginArea.php";
+	////////////////////////////////////////////////////// Page content file //////////////////////////////////////////////////////
+	include_once "PHP - HTML pages content/index.php";
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	include_once "PHP - HTML static content/htmlBody_footer.php";
+	include_once "PHP - HTML static content/htmlBody_bottom.php";
+?>
