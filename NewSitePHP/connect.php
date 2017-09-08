6b1a8624057fd8905e4755c8f03cbe359bb53f3f
naor <naorpio15@gmail.com>
2017-09-08 20:20:53 +0300

    Delete connect.php

--- NewSitePHP/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$connect = mysqli_connect('localhost', 'id2047781_mybookssite', '12345678', 'id2047781_users');
-	if(mysqli_connect_errno()) {
-		die("Conection Failed: " . mysqli_connect_error());
-	}
-?>
