e846ffbef614a3bc8ff31a59c11f98cfa590dd41
naor <naorpio15@gmail.com>
2017-09-08 20:21:26 +0300

    Delete index.php

--- NewSitePHP/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	include_once 'head.php';
-	include_once 'login.php';
-	include_once 'whatnew.php';
-	include_once 'content.php';
-	include_once 'footer.php';
-	include 'registerhtml.php';
-?>
