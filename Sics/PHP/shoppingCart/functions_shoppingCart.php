20f71933dd857521c9aa0ac85137695c33224d8c
naor <naorpio15@gmail.com>
2017-09-29 13:03:37 +0300

    Add files via upload

--- Sics/PHP/shoppingCart/functions_shoppingCart.php
+++ Sics/PHP/shoppingCart/functions_shoppingCart.php
@@ -28,15 +28,29 @@
 	function isItemAlreadyInShoppingCart($itemId) {
 		if (isset($_COOKIE['shoppingCart_data'])) {
 			$cookieValue = $_COOKIE['shoppingCart_data'];
-			if ($cookieValue != '') {
-				$splittedCookieValueArray = explode(',', $cookieValue);
-				for ($i = 0; $i < count($splittedCookieValueArray) - 1; $i++) {
-					if ($splittedCookieValueArray[$i] == $itemId)
-						return true;
-				}
+			$splittedCookieValueArray = explode(',', $cookieValue);
+			for ($i = 0; $i < count($splittedCookieValueArray) - 1; $i++) {
+				if ($splittedCookieValueArray[$i] == $itemId)
+					return true;
 			}
 		}
 		return false;
+	}
+	
+	function getSqlWhereConditionStringOfShoppingCartItems() {
+		if (isset($_COOKIE['shoppingCart_data'])) {
+			$shoppingCartItemsString = '';
+			$cookieValue = $_COOKIE['shoppingCart_data'];
+			$splittedCookieValueArray = explode(',', $cookieValue);
+			for ($i = 0; $i < count($splittedCookieValueArray) - 1; $i++) {
+				if ($i == count($splittedCookieValueArray) - 2)
+					$shoppingCartItemsString .= 'product_number=' . $splittedCookieValueArray[$i];
+				else
+					$shoppingCartItemsString .= 'product_number=' . $splittedCookieValueArray[$i] . ' OR ';
+			}
+			return $shoppingCartItemsString;
+		}
+		return null;
 	}
 	
 	function getShoppingCartItemsCount() {
